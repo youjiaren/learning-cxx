e856dd697c720aa3de547aca7543354138d66c86
陈耀文 <3191016367@qq.com>
2026-07-28 16:27:05 +0800

    Modify answers vector with new resource data
    Updated answers vector to include resource lifecycle analysis results.

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -49,13 +49,11 @@ int main(int argc, char **argv) {
 
     // ---- 不要修改以上代码 ----
 
-    std::vector<const char *> answers[]{
-        {"fd"},
-        // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
-    };
+   std::vector<const char *> answers[]{
+    {"fd"},
+    {"ffr", "d"},
+    {"r", "d", "d"},
+};
 
     // ---- 不要修改以下代码 ----
 
