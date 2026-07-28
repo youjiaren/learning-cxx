27045ed625d9025eb0d3d98e059fafd3f9bfb725
陈耀文 <3191016367@qq.com>
2026-07-28 15:53:01 +0800

    Define variable x and include iostream
    Added variable definition for x and included iostream for input/output.

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -1,10 +1,11 @@
 #include "../exercise.h"
+#include <iostream>  // 补充标准IO头文件
 
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
 
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
-    // x ?
+    int x = 5; // 定义整型变量x并赋值，数值可自行修改
     std::cout << x << " + " << x << " = " << x + x << std::endl;
     return 0;
 }
