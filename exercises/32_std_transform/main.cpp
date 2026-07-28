35405b546aeca1dfa1bb84a411260e87ca5aba60
陈耀文 <3191016367@qq.com>
2026-07-28 16:28:58 +0800

    Implement transformation of vector elements
    Use std::transform to double values and convert to strings

--- exercises/32_std_transform/main.cpp
+++ exercises/32_std_transform/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <sstream>
 
 // READ: `std::transform` <https://zh.cppreference.com/w/cpp/algorithm/transform>
 // READ: `std::vector::begin` <https://zh.cppreference.com/w/cpp/container/vector/begin>
@@ -9,7 +10,11 @@
 int main(int argc, char **argv) {
     std::vector<int> val{8, 13, 21, 34, 55};
     // TODO: 调用 `std::transform`，将 `v` 中的每个元素乘以 2，并转换为字符串，存入 `ans`
-    // std::vector<std::string> ans
+    std::vector<std::string> ans(val.size());
+    std::transform(val.begin(), val.end(), ans.begin(), [](int x) {
+        return std::to_string(x * 2);
+    });
+
     ASSERT(ans.size() == val.size(), "ans size should be equal to val size");
     ASSERT(ans[0] == "16", "ans[0] should be 16");
     ASSERT(ans[1] == "26", "ans[1] should be 26");
