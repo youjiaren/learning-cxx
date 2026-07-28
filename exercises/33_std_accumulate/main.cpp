2733d345500f19fb0c32d3b983a2bc56328eb217
陈耀文 <3191016367@qq.com>
2026-07-28 16:29:51 +0800

    Implement tensor size calculation in main.cpp
    Calculate tensor size using std::accumulate.

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -6,12 +6,10 @@
 int main(int argc, char **argv) {
     using DataType = float;
     int shape[]{1, 3, 224, 224};
-    // TODO: 调用 `std::accumulate` 计算：
-    //       - 数据类型为 float；
-    //       - 形状为 shape；
-    //       - 连续存储；
-    //       的张量占用的字节数
-    // int size =
+    // TODO: 调用 `std::accumulate` 计算张量占用字节数
+    size_t elem_num = std::accumulate(std::begin(shape), std::end(shape), size_t(1), std::multiplies<>());
+    int size = static_cast<int>(elem_num * sizeof(DataType));
+
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
