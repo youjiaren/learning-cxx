83d7aac9e750973f1fd00b12119dbe11a790fe42
陈耀文 <3191016367@qq.com>
2026-07-28 16:05:04 +0800

    Change get method to be const qualified

--- exercises/12_method_const/main.cpp
+++ exercises/12_method_const/main.cpp
@@ -1,11 +1,11 @@
 #include "../exercise.h"
 
 // READ: 有 cv 限定符的成员函数 <https://zh.cppreference.com/w/cpp/language/member_functions>
-
 struct Fibonacci {
     int numbers[11];
     // TODO: 修改方法签名和实现，使测试通过
-    int get(int i) {
+    int get(int i) const {
+        return numbers[i];
     }
 };
 
