dfae9a9b2b7b44ad4f4620e13aa1b3c14c300d38
陈耀文 <3191016367@qq.com>
2026-07-28 15:59:46 +0800

    Add Fibonacci validation logic in is_fibonacci function
    Implement Fibonacci sequence validation using pointers.

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -3,8 +3,17 @@
 // READ: 数组向指针退化 <https://zh.cppreference.com/w/cpp/language/array#%E6%95%B0%E7%BB%84%E5%88%B0%E6%8C%87%E9%92%88%E7%9A%84%E9%80%80%E5%8C%96>
 bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
-    // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
-    // arr[i + 2] = arr[i] + arr[i + 1]
+    // 遍历所有需要校验的三元组
+    for (int i = 0; i <= len - 3; ++i) {
+        // 取 seq[i], seq[i+1], seq[i+2]
+        int a = *(ptr + i * stride);
+        int b = *(ptr + (i + 1) * stride);
+        int c = *(ptr + (i + 2) * stride);
+        // 不满足斐波那契递推式，直接返回false
+        if (c != a + b) {
+            return false;
+        }
+    }
     return true;
 }
 
