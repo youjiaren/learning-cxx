b00ad37b886f74e2a6eb99b4f3e80b82155f7ae2
陈耀文 <3191016367@qq.com>
2026-07-28 16:07:02 +0800

    Implement dynamic constructor and destructor for DynFibonacci

--- exercises/14_class_destruct/main.cpp
+++ exercises/14_class_destruct/main.cpp
@@ -1,8 +1,8 @@
 #include "../exercise.h"
+#include <iostream>
 
 // READ: 析构函数 <https://zh.cppreference.com/w/cpp/language/destructor>
 // READ: RAII <https://learn.microsoft.com/zh-cn/cpp/cpp/object-lifetime-and-resource-management-modern-cpp?view=msvc-170>
-
 /// @brief 任意缓存容量的斐波那契类型。
 /// @details 可以在构造时传入缓存容量，因此需要动态分配缓存空间。
 class DynFibonacci {
@@ -11,14 +11,19 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci(){
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached < i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
