ae4e7a1351a308e451308196bfb371b82dbef42a
陈耀文 <3191016367@qq.com>
2026-07-28 16:01:16 +0800

    Refactor comments and fix URLs in main.cpp
    Updated comments and fixed URLs for clarity.

--- exercises/09_enum&union/main.cpp
+++ exercises/09_enum&union/main.cpp
@@ -1,7 +1,6 @@
 #include "../exercise.h"
 
 // READ: 枚举类型 <https://zh.cppreference.com/w/cpp/language/enum>
-
 // `enum` 是 C 的兼容类型，本质上其对应类型的常量。
 // 在 `enum` 中定义标识符等价于定义 constexpr 常量，
 // 这些标识符不需要前缀，可以直接引用。
@@ -14,7 +13,7 @@ enum ColorEnum : unsigned char {
 };
 
 // 有作用域枚举型是 C++ 引入的类型安全枚举。
-// 其内部标识符需要带前缀引用，如 `Color::Red`。
+// 其内部标识符需要带前缀引用，如 `Color::Red`.
 // 作用域枚举型可以避免命名空间污染，并提供类型安全保证。
 enum class Color : int {
     Red = COLOR_RED,
@@ -24,12 +23,12 @@ enum class Color : int {
 };
 
 ColorEnum convert_by_pun(Color c) {
-    // READ: <https://zh.cppreference.com/w/cpp/language/union>
+    // READ: https://zh.cppreference.com/w/cpp/language/union
     // `union` 表示在同一内存位置存储的不同类型的值。
     // 其常见用法是实现类型双关转换，即将一种类型的值转换为另一种无关类型的值。
     // 但这种写法实际上仅在 C 语言良定义，在 C++ 中是未定义行为。
     // 这是比较少见的 C++ 不与 C 保持兼容的特性。
-    // READ: 类型双关 <https://tttapa.github.io/Pages/Programming/Cpp/Practices/type-punning.html>
+    // READ: 类型双关 https://ttttapa.github.io/Pages/Programming/Cpp/Practices/type-punning.html
     union TypePun {
         ColorEnum e;
         Color c;
@@ -37,7 +36,7 @@ ColorEnum convert_by_pun(Color c) {
 
     TypePun pun;
     // TODO: 补全类型双关转换
-
+    pun.c = c;
     return pun.e;
 }
 
